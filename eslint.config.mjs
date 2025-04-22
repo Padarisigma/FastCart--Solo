98a096a1d7bb6aa195efb155689043a7e7d0a1e1
Izzat <doronshozoda.i@icloud.com>
2025-04-23 01:43:45 +0300

    finish

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-disable-directive': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }
 ];

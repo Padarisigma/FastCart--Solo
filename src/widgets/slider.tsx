7b944019a00fdb873bfb8721b2e61388d72d343e
Izzat <doronshozoda.i@icloud.com>
2025-05-04 13:55:55 +0300

    change fotos

--- src/widgets/slider.tsx
+++ src/widgets/slider.tsx
@@ -29,12 +29,21 @@ const Slider = () => {
 	<SwiperSlide>
 	<Image src='/b74fdd3cb4f7af8d17bfb3451c0e947b.jpg' width={1000} height={1000} alt='image'/>
 	</SwiperSlide>
-	<SwiperSlide><Image src='/8d95ac4350c0a45243a5bfa96d0284a9.jpg' width={1000} height={1000} alt='image'/></SwiperSlide>
+	<SwiperSlide><Image src='/6778e82eb04194173cfc18208453ea58.jpg' width={1000} height={1000} alt='image'/></SwiperSlide>
 	<SwiperSlide>
-	<Image src='/f3759df705f20413b9911252cc263cb9.jpg' width={800} height={800} alt='image'/>
+	<Image src='/77ebc0b9876fa4427d8e52a2b62de082.jpg' width={800} height={800} alt='image'/>
 	</SwiperSlide>
 	<SwiperSlide>
-	<Image src='/e8b74238880bd9d67ec728cff79415e0.jpg' width={800} height={800} alt='image'/>
+	<Image src='/c21303db05008457e27be30318f809a1.jpg' width={800} height={800} alt='image'/>
+	</SwiperSlide>
+	<SwiperSlide>
+	<Image src='/81ad6a2f1135f5586ac2c0ffd114c53f.jpg' width={800} height={800} alt='image'/>
+	</SwiperSlide>
+	<SwiperSlide>
+	<Image src='/66e74c18abaf5ee575544fcc8a9f7df7.jpg' width={800} height={800} alt='image'/>
+	</SwiperSlide>
+	<SwiperSlide>
+	<Image src='/8f86b261ed9a58e521ecafe640ea9bcc.jpg' width={800} height={800} alt='image'/>
 	</SwiperSlide>
 	<SwiperSlide>
 	<Image src='/65eeff4c209bf9c7badcd8a688136363.jpg' width={800} height={800} alt='image'/>
